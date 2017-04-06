7212f63ccb8fa2c1af6b348026f4f0dfbbd79c15
Clase <Clase>
2017-04-05 19:32:56 -0500

    segundo commit

---
+++ 2012113194/2012113194/Cinturon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012113194
+{
+    class Cinturon
+    {
+        public string NumSerie(string numero)
+        {
+            _numero = numero;
+        }
+
+        public string Numero
+        {
+            get { return _numero; }
+        }
+    }
+}
